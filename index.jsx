50b84a3baee5899587f498455b9f0c4b082c2a2b
Kunal Verma <84903187+kv272@users.noreply.github.com>
2023-08-19 22:41:15 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {  
+  return (
+    <h1>Hello Mom!</h1>
+  )
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// ReactDOM.render(<h1>help me bro</h1>,document.getElementById("root"));.
